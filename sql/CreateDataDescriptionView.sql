d57f36c6d2b2e36d49092c5b9d6ddec335b53c5b
twipsii <twipsy92+github@hotmail.com>
2024-09-26 06:08:01 +0200

    Removed unneeded content

--- sql/CreateDataDescriptionView.sql
+++ sql/CreateDataDescriptionView.sql
@@ -1,3 +1,5 @@
+-- Script to create a view to describe the columns in the tables
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,17 +8,13 @@ GO
 CREATE VIEW [dbo].[vw_DataDescription] AS
 
 WITH CTE_COLUMN_INFO_AND_CHECK_CONSTRAINTS AS (
-    -- Retrieve information about columns from all tables in database 'www_db'
+    -- Retrieve information about columns from all tables in database
     SELECT DISTINCT
         SCHEMA_NAME(st.[schema_id]) + '.' + st.[name] AS [Table Name],
-        --ISNULL(con.[definition], '') AS [Check Constraint],
         sc.[name] AS [Column Name],			
         isc.DATA_TYPE AS [Data Type],
         sc.max_length AS [Max Length (Bytes)],
-        --CASE WHEN sc.is_computed = 1 THEN 'Yes' ELSE 'No' END AS [Is Computed],
-        --CASE WHEN syscc.is_persisted = 1 THEN 'Yes' ELSE 'No' END AS [Is Persisted],
         CASE WHEN isc.IS_NULLABLE = 'YES' then 'Yes' else 'No' END AS [Is Nullable],
-        --ISNULL(isc.COLUMN_DEFAULT, '') AS [Default Value],
         ISNULL(ep.[value], 'N/A') AS [Description]		
     FROM sys.tables AS st	
     INNER JOIN sys.columns AS sc 
@@ -29,31 +27,8 @@ WITH CTE_COLUMN_INFO_AND_CHECK_CONSTRAINTS AS (
         ON con.parent_column_id = sc.column_id AND con.parent_object_id = sc.[object_id]
     LEFT JOIN sys.computed_columns AS syscc 
         ON syscc.object_id = sc.object_id AND syscc.[name] = sc.[name]
-)
-/*
-, CTE_FOREIGN_KEY AS (
-    -- This Select Statement retrieves all the data about the Foreign Keys in my DB
-    SELECT 
-        pk_tab.[object_id],
-        SCHEMA_NAME(fk_tab.[schema_id]) + '.' + fk_tab.[name] AS foreign_table,        		
-            'Foreign key' AS [Foreign Key],
-            fk.[name] AS fk_constraint_name,		
-        COL_NAME(fk_cols.parent_object_id,fk_cols.parent_column_id) + 
-        ' references ' + 
-        COL_NAME(fk_cols.referenced_object_id,fk_cols.referenced_column_id) + 
-        ' from ' +
-        SCHEMA_NAME(pk_tab.[schema_id]) + '.' +  pk_tab.[name] AS Details,
-        COL_NAME(fk_cols.referenced_object_id,fk_cols.referenced_column_id) AS ColumnName
-    FROM sys.foreign_keys AS fk
-    INNER JOIN sys.tables AS fk_tab
-        ON fk_tab.[object_id] = fk.parent_object_id
-    INNER JOIN sys.tables AS pk_tab
-        ON pk_tab.[object_id] = fk.referenced_object_id
-    INNER JOIN sys.foreign_key_columns AS fk_cols
-        ON fk_cols.constraint_object_id = fk.[object_id]
-)
-*/
-, CTE_PRIMARY_KEYS AS (
+),
+CTE_PRIMARY_KEYS AS (
     -- Retrieve primary keys for each table
     SELECT 
         t.[object_id],
@@ -80,14 +55,9 @@ WITH CTE_COLUMN_INFO_AND_CHECK_CONSTRAINTS AS (
     AND t.is_ms_shipped <> 1
 )
 SELECT 
-    CICC.*, 
-    --ISNULL(FK.Details,'') AS [Foreign Key Constraint],
+    CICC.*,
     PK.details AS [Primary Key Constraint]
 FROM CTE_COLUMN_INFO_AND_CHECK_CONSTRAINTS AS CICC
-/*
-LEFT OUTER JOIN CTE_FOREIGN_KEYS AS FK 
-    ON CICC.[Column Name]=FKT.ColumnName AND CICC.[Table Name] = FKT.foreign_table
-*/
 INNER JOIN CTE_PRIMARY_KEYS AS PK
     ON CICC.[Table Name] = PK.table_view
 
