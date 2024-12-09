3dbfb12005ec56e02753a8fdbe8db7a70a48fae3
Thomas Nemeth <thomas.nemeth@posteo.de>
2024-12-09 17:14:28 +0100

    Server anpassungen

---
+++ scripts/run_weather_forecast.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Initialisiere conda
+source /home/tom/anaconda3/etc/profile.d/conda.sh
+
+# Aktiviere die Conda-Umgebung
+conda activate www
+
+# Führe das Python-Skript aus
+python /home/tom/Schreibtisch/Wanderwege/scripts/weather_forecast.py
